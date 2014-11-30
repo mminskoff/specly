cbe817a23f1663868212969c41e2e110da9258a6
mminskoff <mminskoff@mac.com>
2014-11-30 14:45:41 -0500

    first commit

---
+++ specly.js
@@ -0,0 +1,22 @@
+$('body').scrollspy({
+    target: '.bs-docs-sidebar',
+    offset: 20
+});
+$("#sidebar").affix({
+    offset: {
+      top: 60
+    }
+});
+
+
+var pendingState 
+$(window).on('activate.bs.scrollspy', function (e) {
+	if (!pendingState) {
+		pendingState = $("a[href^='#']", e.target).attr("href");
+		setTimeout(function() {
+			history.replaceState({}, "", pendingState);
+			pendingState = null;
+		}, 0);
+	}
+});
+
